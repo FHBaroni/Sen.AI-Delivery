b988790a06abc7b918999f0754732f6e10af4a93
Fernando Henrique Baroni <fhbaroni@gmail.com>
2025-09-26 20:39:47 -0300

    Add Plataforma e colisores

--- Sen.AI/Assets/Scripts/Drone.cs
+++ Sen.AI/Assets/Scripts/Drone.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,11 @@ public class Drone : MonoBehaviour
     private void Awake()
     {
         droneRb = GetComponent<Rigidbody2D>();
+
+        // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, 1)));
+        // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(.5f, .5f)));
+        // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(1, 0)));
+        // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, -1)));
     }
     private void FixedUpdate()
     {
@@ -26,5 +32,29 @@ public class Drone : MonoBehaviour
             float turnSpeed = 100f;
             droneRb.AddTorque(turnSpeed * Time.deltaTime);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (!collision2D.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Fora da plataforma");
+            return;
+        }
+
+        //Debug.Log(collision2D.relativeVelocity.magnitude);
+        float crashMagnitude = 3f;
+        if (collision2D.relativeVelocity.magnitude > crashMagnitude)
+        {
+            Debug.Log("Quebrou ao pousar");
+            return;
+        }
+        float dotVector = Vector2.Dot(Vector2.up, transform.up); //Dot product == produto escalar
+        float minDotVector = 0.90f;
+        if (dotVector < minDotVector)
+        {
+            Debug.Log("Muito inclinado para pousar");
+            return;
+        }
+        Debug.Log("Pousou corretamente");
     }
 }
